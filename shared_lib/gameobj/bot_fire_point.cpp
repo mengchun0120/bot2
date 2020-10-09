030ba4527a816a832ac466f95883a43282141d13
Chun <mengchun0120@gmail.com>
2020-10-09 09:30:09 -0400

    Fixed a bug

--- shared_lib/gameobj/bot_fire_point.cpp
+++ shared_lib/gameobj/bot_fire_point.cpp
@@ -5,8 +5,11 @@ namespace bot {
 
 FirePoint::FirePoint()
 {
-    std::fill(m_firePos, m_firePos + sizeof(m_firePos), 0.0f);
-    std::fill(m_fireDirection, m_fireDirection + sizeof(m_fireDirection), 0.0f);
+    for(unsigned int i = 0; i < Constants::NUM_FLOATS_PER_POSITION; ++i)
+    {
+        m_firePos[i] = 0.0f;
+        m_fireDirection[i] = 0.0f;
+    }
 }
 
 FirePoint::FirePoint(const FirePoint& fp)
@@ -16,9 +19,11 @@ FirePoint::FirePoint(const FirePoint& fp)
 
 FirePoint& FirePoint::operator=(const FirePoint& fp)
 {
-    std::copy(fp.m_firePos, fp.m_firePos + sizeof(fp.m_firePos), m_firePos);
-    std::copy(fp.m_fireDirection, fp.m_fireDirection + sizeof(fp.m_fireDirection),
-              m_fireDirection);
+    for(unsigned int i = 0; i < Constants::NUM_FLOATS_PER_POSITION; ++i)
+    {
+        m_firePos[i] = fp.m_firePos[i];
+        m_fireDirection[i] = fp.m_fireDirection[i];
+    }
     return *this;
 }
 
